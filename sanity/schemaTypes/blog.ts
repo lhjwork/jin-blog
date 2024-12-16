5b69e4ba0ef571fab42533bed55a203a3465f193
lhjwork <jin.come.up.business@gmail.com>
2024-12-17 00:43:04 +0900

    next create and sanity install and schema setting

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,12 @@
+export default {
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    {name: 'title', type: 'string', title: 'Title of blog article'},
+    {name: 'slug', type: 'slug', title: 'slug of your article'},
+    {name: 'titleImage', type: 'image', title: 'title Image'},
+    {name: 'smallDescription', type: 'text', title: 'Small Description'},
+    {name: 'content', type: 'array', title: 'Content', of: [{type: 'block'}]},
+  ],
+}

9451ac0f340722777208fba0351d895aba49326e
lhjwork <jin.come.up.business@gmail.com>
2024-12-17 17:55:26 +0900

    text 수정

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
     return ( 
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
-      <Link href="/">
+      <Link href="/" className="font-bold text-3xl">
         진이의<span>블로그</span>
       </Link>
     </nav>)

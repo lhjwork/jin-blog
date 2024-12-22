79b491eda9199e3c6f7877d084e5a1240e21fab3
lhjwork <jin.come.up.business@gmail.com>
2024-12-23 00:57:29 +0900

    next 에서 backend 구현 해보기 및 next config에서 이미지 최적화 설정 정의 외부 이미지 도메인을 허용하는 설정

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="w-full relative flex items-center justify-between py-5">
+    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
         진이의<span className="text-primary">블로그</span>
       </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
-import Navbar from "./components/Navbar"
+import { Card } from "@/components/ui/card";
+import { simpleBlogCard } from "./lib/interface";
+import { client, urlFor } from "./lib/sanity";
+import Image from "next/image";
 
-export default function Home() {
+async function getData() {
+  const query = `
+  *[_type == 'blog'] | order(_createdAt desc) {
+  title,
+    smallDescription,
+    "currentSlug": slug.current,
+    titleImage
+}`;
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData();
+  // 56:25
   return (
-  <div>
-    <Navbar />
-    <h1>진이의 블로그</h1>
-  </div>
-  )
+    <div className="grid grid-cols-1 lg:grid-cols-2 mt-5 gap-5">
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt="카드 이미지"
+            width={500}
+            height={500}
+            className="rounded-t-lg h-[200px] object-cover"
+          />
+        </Card>
+      ))}
+    </div>
+  );
 }

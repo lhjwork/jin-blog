7b6155157d67ad766156635a77b9caae0b64a771
lhjwork <jin.come.up.business@gmail.com>
2024-12-23 01:28:43 +0900

    blog 상세페이지 추가 작업 중

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,10 +2,10 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  apiVersion: "2022-03-07",
+  apiVersion: "v2022-03-07",
   dataset: "production",
   projectId: "phik0bc2",
-  useCdn: false,
+  useCdn: false, // 로컬 서버에서 최신 데이터를 가져오기 위해 false로 설정
 });
 
 const builer = imageUrlBuilder(client);
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { simpleBlogCard } from "./lib/interface";
 import { client, urlFor } from "./lib/sanity";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 async function getData() {
   const query = `
@@ -29,6 +31,13 @@ export default async function Home() {
             height={500}
             className="rounded-t-lg h-[200px] object-cover"
           />
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">{post.smallDescription}</p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read more</Link>
+            </Button>
+          </CardContent>
         </Card>
       ))}
     </div>
